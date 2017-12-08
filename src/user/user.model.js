7ed79b6d6fc043125cd59f2fe1994962154529b7
Paola Herrera Dominguez <paola.herrera@globant.com>
2017-12-07 19:14:04 -0500

    Added email to getUsersInformation

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -162,7 +162,8 @@ function getUsersInformation(ids) {
                 id: 1,
                 firstName: 1,
                 lastName: 1,
-                photo: 1
+                photo: 1,
+                email: 1
             })
             .toArray((err, result) => {
                 if (err) { return reject(err); }
